4fe77d8bdca43bb2da7568d9ff93f585a7922f8e
Joost van der Griendt <joostvdg@gmail.com>
2024-05-08 15:54:51 +0200

    finish module 7

--- lab/24-test/src/test/java/rewards/RewardNetworkTests.java
+++ lab/24-test/src/test/java/rewards/RewardNetworkTests.java
@@ -1,12 +1,19 @@
 package rewards;
 
 import common.money.MonetaryAmount;
+import config.RewardsConfig;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -82,34 +89,56 @@ import static org.junit.jupiter.api.Assertions.*;
  *   lab document.)
  * - Run the test again.
  */
-
+@SpringJUnitConfig()
+@ActiveProfiles({"stub", "jndi"})
 public class RewardNetworkTests {
 
-	
+	@Configuration
+	@Import({
+			TestInfrastructureLocalConfig.class,
+			TestInfrastructureJndiConfig.class,
+			RewardsConfig.class })
+	static public class TestInfrastructureConfig {
+
+		/**
+		 * The bean logging post-processor from the bean lifecycle slides.
+		 */
+		@Bean
+		public static LoggingBeanPostProcessor loggingBean(){
+			return new LoggingBeanPostProcessor();
+		}
+	}
+
+
 	/**
 	 * The object being tested.
 	 */
 	private RewardNetwork rewardNetwork;
 
+	@Autowired
+	RewardNetworkTests(RewardNetwork rewardNetwork) {
+		this.rewardNetwork = rewardNetwork;
+	}
+
 	/**
 	 * Need this to enable clean shutdown at the end of the application
 	 */
-	private ConfigurableApplicationContext context;
+//	private ConfigurableApplicationContext context;
 
-	@BeforeEach
-	public void setUp() {
-		// Create the test configuration for the application from one file
-		context = SpringApplication.run(TestInfrastructureConfig.class);
-		// Get the bean to use to invoke the application
-		rewardNetwork = context.getBean(RewardNetwork.class);
-	}
-
-	@AfterEach
-	public void tearDown() throws Exception {
-		// simulate the Spring bean destruction lifecycle:
-		if (context != null)
-			context.close();
-	}
+//	@BeforeEach
+//	public void setUp() {
+//		// Create the test configuration for the application from one file
+//		context = SpringApplication.run(TestInfrastructureConfig.class);
+//		// Get the bean to use to invoke the application
+//		rewardNetwork = context.getBean(RewardNetwork.class);
+//	}
+//
+//	@AfterEach
+//	public void tearDown() throws Exception {
+//		// simulate the Spring bean destruction lifecycle:
+//		if (context != null)
+//			context.close();
+//	}
 
 	@Test
 	@DisplayName("Test if reward computation and distribution works")
