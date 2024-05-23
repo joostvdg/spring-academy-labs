b6edd7d4aa5d5314dc5a967f4456deeb72627df3
Joost van der Griendt <joostvdg@gmail.com>
2024-05-23 21:02:40 +0200

    finish spring security module

--- lab/42-security-rest/src/main/java/config/RestSecurityConfig.java
+++ lab/42-security-rest/src/main/java/config/RestSecurityConfig.java
@@ -4,6 +4,8 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.core.userdetails.User;
@@ -17,7 +19,12 @@ import org.springframework.security.web.SecurityFilterChain;
 // - Add @EnableMethodSecurity annotation to this class
 
 @Configuration
+@EnableMethodSecurity
 public class RestSecurityConfig {
+
+	private static final String ADMIN = "ADMIN";
+	private static final String SUPER_ADMIN = "SUPERADMIN";
+	private static final String USER = "USER";
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -35,6 +42,11 @@ public class RestSecurityConfig {
                 //   for all roles - "USER", "ADMIN", "SUPERADMIN"
 
                 // Deny any request that doesn't match any authorization rule
+				.requestMatchers(HttpMethod.DELETE, "/accounts/**").hasRole(SUPER_ADMIN)
+				.requestMatchers(HttpMethod.POST, "/accounts/**").hasAnyRole(ADMIN, SUPER_ADMIN)
+				.requestMatchers(HttpMethod.PUT, "/accounts/**").hasAnyRole(ADMIN, SUPER_ADMIN)
+				.requestMatchers(HttpMethod.GET, "/accounts/**").hasAnyRole(ADMIN, SUPER_ADMIN, USER)
+				.requestMatchers(HttpMethod.GET, "/authorities").hasAnyRole(ADMIN, SUPER_ADMIN, USER)
                 .anyRequest().denyAll())
         .httpBasic(withDefaults())
         .csrf(CsrfConfigurer::disable);
@@ -46,7 +58,7 @@ public class RestSecurityConfig {
 	// TODO-14b (Optional): Remove the InMemoryUserDetailsManager definition
 	// - Comment the @Bean annotation below
 	
-	@Bean
+	//@Bean
     public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
 
 		// TODO-05: Add three users with corresponding roles:
@@ -55,9 +67,11 @@ public class RestSecurityConfig {
 		// - "superadmin"/"superadmin" with "USER", "ADMIN", and "SUPERADMIN" roles
 		// (Make sure to store the password in encoded form.)
     	// - pass all users in the InMemoryUserDetailsManager constructor
-		UserDetails user = User.withUsername("user").password(passwordEncoder.encode("user")).roles("USER").build();
+		UserDetails user = User.withUsername("user").password(passwordEncoder.encode("user")).roles(USER).build();
+		var adminUser = User.withUsername("admin").password(passwordEncoder().encode("admin")).roles(ADMIN, USER).build();
+		var supserAdminUser = User.withUsername("superadmin").password(passwordEncoder.encode("superadmin")).roles(SUPER_ADMIN, ADMIN, USER).build();
 
-		return new InMemoryUserDetailsManager(user /* Add new users comma-separated here */);
+		return new InMemoryUserDetailsManager(user, adminUser, supserAdminUser);
 	}
     
     @Bean
